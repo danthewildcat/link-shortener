232cbfb1257f04dd60ce9bde945171fe205c1872
Dan Sutich <dan.sutich@getpreo.com>
2018-12-05 12:54:34 -0500

    Add SQL performance TODO

--- src/models.js
+++ src/models.js
@@ -32,6 +32,11 @@ export const Link = sequelize.define('visited_link', {
 });
 
 export async function getVisitedLink(url: string): Promise<LinkType> {
+  // TODO: sequelize does not support a means of doing an upsert with a functional
+  // value for the update. There is definitely a way to do a SELECT and update the value
+  // if it exists using raw SQL. For the sake of pushing out a working prototype this
+  // low performance implementation will work and the fix will be tracked in Github:
+  // https://github.com/danthewildcat/link-shortener/issues/2
   const link = await Link.findOne({
     where: {url},
   });
